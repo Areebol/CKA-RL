794917a287c392b16cf21f83f6adde71c6fe0bd0
Areebol <areebol@qq.com>
2024-11-30 10:09:16 +0800

    Add requirements

--- experiments/atari/run.sh
+++ experiments/atari/run.sh
@@ -1,7 +1,6 @@
-# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-python run_experiments.py --algorithm componet --start-mode 0 --first-mode 0 --last-mode 0
-python run_experiments.py --algorithm finetune --start-mode 0 --first-mode 0 --last-mode 0
-python run_experiments.py --algorithm from-scratch --start-mode 0 --first-mode 0 --last-mode 0
-python run_experiments.py --algorithm prog-net --start-mode 0 --first-mode 0 --last-mode 0
-python run_experiments.py --algorithm packnet --start-mode 0 --first-mode 0 --last-mode 0
+# python run_experiments.py --algorithm tvnet --start-mode 0 --first-mode 0 --last-mode 0
+# python run_experiments.py --algorithm componet --start-mode 0 --first-mode 0 --last-mode 0 --debug True
+# python run_experiments.py --algorithm finetune --start-mode 0 --first-mode 0 --last-mode 0
+# python run_experiments.py --algorithm from-scratch --start-mode 0 --first-mode 0 --last-mode 0
+# python run_experiments.py --algorithm prog-net --start-mode 0 --first-mode 0 --last-mode 0
+# python run_experiments.py --algorithm packnet --start-mode 0 --first-mode 0 --last-mode 0
