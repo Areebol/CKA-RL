7b753f7ee1e99f2dfae3d99c1473b7ac3e38b622
Areebol <103118945+Areebol@users.noreply.github.com>
2024-12-16 10:17:57 +0800

    Add script

--- experiments/atari/search_fusenet.sh
+++ experiments/atari/search_fusenet.sh
@@ -3,10 +3,11 @@ TRAIN_MODES="--first-mode 2 --last-mode 7"
 # python run_experiments.py --method_type FuseNet   --alpha_factor 1e-3 $TRAIN_MODES --seed $SEED --tag "AlphaFactor1e-3"
 # python run_experiments.py --method_type FuseNet   --alpha_factor 1e-4 $TRAIN_MODES --seed $SEED --tag "AlphaFactor1e-4"
 python run_experiments.py --method_type FuseNet \
-                          --alpha_learning_rate 1e-3 \
+                          --alpha_learning_rate 1e-2 \
                           --alpha_factor 1e-3 \
+                          --fix_alpha True 
                           $TRAIN_MODES \
                           --seed $SEED \
-                          --tag "Alpha_lr_1e-3"
+                          --tag "Alpha_lr_1e-2"
                         #   --debug True
 
