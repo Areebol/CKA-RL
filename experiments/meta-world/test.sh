eb7b626bd1d866a926bb920d6527df5de89ecefc
Areebol <103118945+Areebol@users.noreply.github.com>
2025-03-10 10:32:20 +0800

    Final revision

--- experiments/meta-world/test.sh
+++ experiments/meta-world/test.sh
@@ -1,6 +1,7 @@
 TRAIN_MODE=9
 SEED=42
-
+for TRAIN_MODE in 0 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19
+do
 for METHOD in simple finetune prognet packnet masknet creus cbpnet fusenet fusenet_merge
 do
     for ((TEST_MODE=0; TEST_MODE<=TRAIN_MODE; TEST_MODE++))
@@ -12,4 +13,5 @@ do
                                 --csv "data/eval_results.csv" \
                                 --seed $SEED
     done
+done
 done
